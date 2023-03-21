3ae763688aa90b8779e767bfaf2d475b0875d27c
kjh-busan <picsmash7@gmail.com>
2023-03-22 02:24:48 +0900

    feat: form UI input WIP'

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [entereduserName, setEntereduserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+  const changeUserNameHandler = (event) => {
+    setEntereduserName(event.target.value);
+  };
+  const changeAgeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
@@ -14,11 +23,21 @@ const AddUser = (props) => {
         <label className={classes.label} htmlFor="username">
           User Name
         </label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={entereduserName}
+          onChange={changeUserNameHandler}
+        />
         <label className={classes.label} htmlFor="age">
           Age (Years){" "}
         </label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={changeAgeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
